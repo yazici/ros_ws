06d984c089244da2419fedd2cad0ecb12676a719
Yong Wang <wangyongseu9@gmail.com>
2018-08-06 15:51:12 +0200

    fix the git version conflict problem

--- model_loader/src/tf_test.cpp
+++ model_loader/src/tf_test.cpp
@@ -6,21 +6,13 @@ int main(int argc, char** argv){
   ros::init(argc, argv, "tf_test_broadcaster");
   ros::NodeHandle node;
   static tf::TransformBroadcaster br;
-<<<<<<< HEAD
   ros::Rate loop_rate(30);
-=======
-  ros::Rate loop_rate(1);
->>>>>>> 637f1af354450c87a7182e3a7035b73f2f10a831
 
   int count = 0;
   while (ros::ok())
   {
     tf::Transform transform;
-<<<<<<< HEAD
     transform.setOrigin( tf::Vector3(0.0, 0.0, 0.0) );
-=======
-    transform.setOrigin( tf::Vector3(0.0, - count / 20.0, 0.0) );
->>>>>>> 637f1af354450c87a7182e3a7035b73f2f10a831
     tf::Quaternion q;
     q.setRPY(0.0, 0.0, 0.0);
     transform.setRotation(q);
