6ab2eb20f930cd598e4dd9fd20d08b937a5bfb14
Yong Wang <wangyongseu9@gmail.com>
2018-09-04 17:43:58 +0200

    Add launch file [start_motion_control] which will start all necessary service. Add node [control_node] which will control the execution of the whole pipeline. Modify node [move_camera] to include services [start_move_camera] and [stop_move_camera]

---
+++ motion_control/src/control_node.cpp
@@ -0,0 +1,108 @@
+#include <cmath>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+#include <std_srvs/Empty.h>
+#include <ros/package.h>
+#include <tf2/LinearMath/Quaternion.h>
+#include <tf/transform_listener.h>
+#include <geometry_msgs/TransformStamped.h>
+#include <tf2_ros/static_transform_broadcaster.h>
+
+#include <moveit/move_group_interface/move_group_interface.h>
+#include <moveit/planning_scene_interface/planning_scene_interface.h>
+#include <moveit_msgs/DisplayRobotState.h>
+#include <moveit_msgs/DisplayTrajectory.h>
+#include <moveit_msgs/AttachedCollisionObject.h>
+#include <moveit_msgs/CollisionObject.h>
+#include <moveit_visual_tools/moveit_visual_tools.h>
+#include <moveit/trajectory_processing/iterative_time_parameterization.h>
+
+static const std::string PLANNING_GROUP = "camera";
+
+class ControlNode {
+
+  ros::NodeHandle nh_;
+  tf2_ros::StaticTransformBroadcaster br;
+  ros::ServiceClient add_aircraft_frame_, remove_aircraft_frame_;
+  ros::ServiceServer end_rough_localization_, end_profile_scan_;
+
+  boost::shared_ptr< moveit::planning_interface::MoveGroupInterface > move_group;
+  boost::shared_ptr< moveit::planning_interface::PlanningSceneInterface > planning_scene_interface;
+  const robot_state::JointModelGroup* joint_model_group;
+
+public:
+
+  ControlNode ()
+  {
+    add_aircraft_frame_ = nh_.serviceClient < std_srvs::Empty > ( "add_aircraft_frame" );
+    remove_aircraft_frame_ = nh_.serviceClient < std_srvs::Empty > ( "remove_aircraft_frame" );
+    end_rough_localization_ = nh_.advertiseService ( "end_rough_localization", &ControlNode::end_rough_localization, this );
+    end_profile_scan_ = nh_.advertiseService ( "end_profile_scan", &ControlNode::end_profile_scan, this );
+
+    move_group.reset ( new moveit::planning_interface::MoveGroupInterface ( PLANNING_GROUP ) );
+    planning_scene_interface.reset ( new moveit::planning_interface::PlanningSceneInterface () );
+    joint_model_group = move_group->getCurrentState()->getJointModelGroup ( PLANNING_GROUP );
+    ROS_INFO_NAMED ( "move_camera", "Reference frame: %s", move_group->getPlanningFrame().c_str() );
+    ROS_INFO_NAMED ( "move_camera", "End effector link: %s", move_group->getEndEffectorLink().c_str() );
+  }
+
+  ~ControlNode ()
+  {}
+
+  void start_rough_localization ()
+  {
+    std_srvs::Empty msg;
+    if ( add_aircraft_frame_.call ( msg ) )
+    {
+      std::cout << "aircraft frame has been added" << std::endl;
+      std::map < std::string, double > value_map = move_group->getNamedTargetValues ( "pose2" );
+      // std::cout << move_group->getNamedTargetValues ( "pose2" ) << std::endl;
+      // for ( const auto& value_pair : value_map )
+      // {
+      //   std::cout << "<" << value_pair.first << "> = <" << value_pair.second << ">\n";
+      // }
+      move_group->setJointValueTarget ( value_map );
+      moveit::planning_interface::MoveGroupInterface::Plan my_plan;
+      bool success = ( move_group->plan ( my_plan ) == moveit::planning_interface::MoveItErrorCode::SUCCESS );
+      if ( success )
+      {
+        std::cout << "reset robot pose to pose 2" << std::endl;
+        move_group->setMaxVelocityScalingFactor ( 0.1 );
+        move_group->setMaxAccelerationScalingFactor ( 0.1 );
+        move_group->move ();
+      }
+    }
+  }
+
+  bool end_rough_localization ( std_srvs::Empty::Request& req, std_srvs::Empty::Response& res )
+  {
+    /*
+    ** do something
+    */
+    return true;
+  }
+
+  bool end_profile_scan ( std_srvs::Empty::Request& req, std_srvs::Empty::Response& res )
+  {
+    /*
+    ** do something
+    */
+    return true;
+  }
+
+};
+
+int main ( int argc, char** argv )
+{
+  ros::init ( argc, argv, "control_node" );
+  ros::AsyncSpinner spinner ( 4 );
+  spinner.start ();
+  ControlNode control_node;
+  control_node.start_rough_localization ();
+  ros::waitForShutdown ();
+  return 0;
+}
--- motion_control/src/move_camera.cpp
+++ motion_control/src/move_camera.cpp
@@ -1,62 +1,93 @@
-#include <moveit/move_group_interface/move_group_interface.h>
-#include <moveit/planning_scene_interface/planning_scene_interface.h>
+#include <cmath>
+#include <iostream>
 
-#include <moveit_msgs/DisplayRobotState.h>
-#include <moveit_msgs/DisplayTrajectory.h>
-
-#include <moveit_msgs/AttachedCollisionObject.h>
-#include <moveit_msgs/CollisionObject.h>
-
-#include <moveit_visual_tools/moveit_visual_tools.h>
-
+#include <std_srvs/Empty.h>
 #include <tf2/LinearMath/Quaternion.h>
 #include <tf/transform_listener.h>
 
-#include <cmath>
+#include <moveit/move_group_interface/move_group_interface.h>
+#include <moveit_msgs/DisplayRobotState.h>
+#include <moveit_msgs/DisplayTrajectory.h>
+#include <moveit_msgs/AttachedCollisionObject.h>
+#include <moveit_msgs/CollisionObject.h>
+#include <moveit_visual_tools/moveit_visual_tools.h>
 
-int main(int argc, char** argv)
+bool is_move = false;
+
+void move_camera ()
 {
-  ros::init( argc, argv, "motion_control" );
-  ros::NodeHandle node_handle;
-  ros::AsyncSpinner spinner(4);
-  spinner.start();
-
-  // create interface for motion planning
   static const std::string PLANNING_GROUP = "camera";
-  moveit::planning_interface::MoveGroupInterface move_group( PLANNING_GROUP );
-  moveit::planning_interface::PlanningSceneInterface planning_scene_interface;
-  const robot_state::JointModelGroup* joint_model_group = move_group.getCurrentState()->getJointModelGroup(PLANNING_GROUP);
-  ROS_INFO_NAMED( "motion_ctrl", "Reference frame: %s", move_group.getPlanningFrame().c_str() );
-  ROS_INFO_NAMED( "motion_ctrl", "End effector link: %s", move_group.getEndEffectorLink().c_str() );
-  ros::Rate rate( 0.5 );
+  moveit::planning_interface::MoveGroupInterface move_group ( PLANNING_GROUP );
+  const robot_state::JointModelGroup* joint_model_group = move_group.getCurrentState()->getJointModelGroup ( PLANNING_GROUP );
+  ROS_INFO_NAMED ( "move_camera", "Reference frame: %s", move_group.getPlanningFrame().c_str() );
+  ROS_INFO_NAMED ( "move_camera", "End effector link: %s", move_group.getEndEffectorLink().c_str() );
+  ros::Rate rate ( 2 );
 
-  while ( ros::ok() )
+  while ( ros::ok () )
   {
-    moveit::core::RobotStatePtr current_state = move_group.getCurrentState();
-    std::vector<double> joint_group_positions;
-    current_state->copyJointGroupPositions( joint_model_group, joint_group_positions );
-    std::cout << joint_group_positions[3] << std::endl;
-    if ( std::abs( joint_group_positions[3] + 1.2994 ) <= 0.01 )
+    // std::cout << "is_move = " << is_move << std::endl;
+    if ( is_move )
     {
-      joint_group_positions[3] = -0.069;  // radians
-    } else if ( std::abs( joint_group_positions[3] + 0.069 ) <= 0.01 )
-    {
-        joint_group_positions[3] = -1.2994;  // radians
+      moveit::core::RobotStatePtr current_state = move_group.getCurrentState();
+      std::vector < double > joint_group_positions;
+      current_state->copyJointGroupPositions ( joint_model_group, joint_group_positions );
+      std::cout << "current angle: " << joint_group_positions[3] << std::endl;
+      // if the start robot pose is pose 2
+      if ( std::abs ( joint_group_positions[3] + 1.2994 ) <= 0.01 )
+      {
+        int move_counter = 0;
+        while ( move_counter != 2 )
+        {
+          if ( move_counter  == 0 )
+          {
+            // set pose 3 in radians
+            joint_group_positions[3] = -0.069;
+          } else if ( move_counter  == 1 )
+          {
+            // set pose 2 in radians
+            joint_group_positions[3] = -1.2994;
+          }
+          move_group.setJointValueTarget ( joint_group_positions );
+          moveit::planning_interface::MoveGroupInterface::Plan my_plan;
+          bool success = ( move_group.plan ( my_plan ) == moveit::planning_interface::MoveItErrorCode::SUCCESS );
+          if ( success )
+          {
+            std::cout << "move to: " << joint_group_positions[3] << std::endl;
+            move_group.setMaxVelocityScalingFactor ( 0.01 );
+            move_group.setMaxAccelerationScalingFactor ( 0.01 );
+            move_group.move ();
+          }
+          move_counter ++;
+        }
+      }
     }
-    move_group.setJointValueTarget(joint_group_positions);
-
-    moveit::planning_interface::MoveGroupInterface::Plan my_plan;
-    bool success = ( move_group.plan(my_plan) == moveit::planning_interface::MoveItErrorCode::SUCCESS );
-    if (success)
-    {
-      move_group.setMaxVelocityScalingFactor(0.01);
-      move_group.setMaxAccelerationScalingFactor(0.01);
-      move_group.move();
-    }
-
     rate.sleep();
   }
+}
 
+bool start_move_camera ( std_srvs::Empty::Request& req, std_srvs::Empty::Response& res )
+{
+  is_move = true;
+  return true;
+}
+
+bool stop_move_camera ( std_srvs::Empty::Request& req, std_srvs::Empty::Response& res )
+{
+  is_move = false;
+  return true;
+}
+
+int main ( int argc, char** argv )
+{
+  ros::init ( argc, argv, "move_camera" );
+  ros::NodeHandle nh_;
+  ros::AsyncSpinner spinner ( 4 );
+  spinner.start ( );
+  ros::ServiceServer start_move_camera_, stop_move_camera_;
+  start_move_camera_ = nh_.advertiseService ( "start_move_camera", &start_move_camera );
+  stop_move_camera_ = nh_.advertiseService ( "stop_move_camera", &stop_move_camera );
+  is_move = false;
+  move_camera ();
   ros::shutdown();
   return 0;
 }
