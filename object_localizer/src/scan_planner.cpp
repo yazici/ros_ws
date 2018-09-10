8d75f890546546f82562a072689fc02581805bec
Lasse <l.wendland@synergeticon.com>
2018-09-10 14:13:34 +0200

    add services [start_scan_planner] and [stop_scan_planner] to node [scan_planner]

--- object_localizer/src/scan_planner.cpp
+++ object_localizer/src/scan_planner.cpp
@@ -1,10 +1,14 @@
 #include <stdio.h>
 #include <iostream>
+#include <fstream>
 #include <string>
 #include <ctime>
 
 #include <ros/ros.h>
+#include <std_srvs/Empty.h>
 #include <sensor_msgs/PointCloud2.h>
+#include <tf2/LinearMath/Quaternion.h>
+#include <tf/transform_listener.h>
 
 // converse header file between ros points2 and pcl point cloud
 #include <pcl_conversions/pcl_conversions.h>
@@ -12,9 +16,6 @@
 #include "object_localizer_msg/BBox_float.h"
 #include "object_localizer_msg/BBox_list.h"
 #include "object_localizer_msg/Segment_list.h"
-
-#include <tf2/LinearMath/Quaternion.h>
-#include <tf/transform_listener.h>
 
 // PCL head files
 #include <pcl/io/ply_io.h>
@@ -28,6 +29,8 @@
 #include <pcl/visualization/pcl_visualizer.h>
 #include <pcl/features/normal_3d.h>
 #include <pcl/common/transforms.h>
+
+std::string reference_frame = "world";
 
 typedef pcl::PointXYZRGB PointT;
 typedef pcl::PointCloud< PointT > PointCloudT;
@@ -51,7 +54,7 @@ void show_segment_cloud ( PointCloudT::ConstPtr cloud )
   }
 }
 
-void getMinMax3D (const pcl::PointCloud<PointT> &cloud, PointT &min_pt, PointT &max_pt)
+void getMinMax3D ( const pcl::PointCloud<PointT> &cloud, PointT &min_pt, PointT &max_pt )
 {
   Eigen::Array4f min_p, max_p;
   min_p.setConstant (FLT_MAX);
@@ -86,7 +89,7 @@ void getMinMax3D (const pcl::PointCloud<PointT> &cloud, PointT &min_pt, PointT &
   max_pt.x = max_p[0]; max_pt.y = max_p[1]; max_pt.z = max_p[2];
 }
 
-void calculate_bounding_box( PointCloudT::ConstPtr cloudSegmented )
+void calculate_bounding_box ( PointCloudT::ConstPtr cloudSegmented )
 {
   // Compute principal directions
   Eigen::Vector4f pcaCentroid;
@@ -140,7 +143,7 @@ template <typename T> int sgn(T val) {
     return (T(0) < val) - (val < T(0));
 }
 
-float calculate_theta ( PointCloudT::ConstPtr cloudSegmented, Eigen::Vector3f& central_point)
+float calculate_theta ( PointCloudT::ConstPtr cloudSegmented, Eigen::Vector3f& central_point )
 {
   // Compute principal directions
   Eigen::Vector4f pcaCentroid;
@@ -155,31 +158,31 @@ float calculate_theta ( PointCloudT::ConstPtr cloudSegmented, Eigen::Vector3f& c
   Eigen::Matrix3f eigenVectorsPCA = eigen_solver.eigenvectors();
   // This line is necessary for proper orientation in some cases. The numbers come out the same without it, but
   // the signs are different and the box doesn't get correctly oriented in some cases.
-  eigenVectorsPCA.col(2) = eigenVectorsPCA.col(0).cross(eigenVectorsPCA.col(1));
+  eigenVectorsPCA.col ( 2 ) = eigenVectorsPCA.col ( 0 ).cross ( eigenVectorsPCA.col ( 1 ) );
 
-  std::cout << "eigen vector 0: [" << eigenVectorsPCA(0, 0) << ", " << eigenVectorsPCA(1, 0) << ", " << eigenVectorsPCA(2, 0) << "]" << std::endl;
-  if ( std::abs( eigenVectorsPCA( 0, 0 ) ) < 0.1 &&  sgn<float>( eigenVectorsPCA( 1, 0 ) ) == sgn<float>( eigenVectorsPCA( 2, 0 ) ) )
+  std::cout << "eigen vector 0: [" << eigenVectorsPCA (0, 0) << ", " << eigenVectorsPCA (1, 0) << ", " << eigenVectorsPCA (2, 0) << "]" << std::endl;
+  if ( std::abs( eigenVectorsPCA ( 0, 0 ) ) < 0.1 &&  sgn<float>( eigenVectorsPCA ( 1, 0 ) ) == sgn<float>( eigenVectorsPCA ( 2, 0 ) ) )
   {
-    float y = std::abs( eigenVectorsPCA( 1, 0 ) );
-    float z = std::abs( eigenVectorsPCA( 2, 0 ) );
+    float y = std::abs( eigenVectorsPCA ( 1, 0 ) );
+    float z = std::abs( eigenVectorsPCA ( 2, 0 ) );
     float theta = atan2 ( z, y ) * 180.0 / M_PI + 90.0;
     return theta;
   }
 
-  std::cout << "eigen vector 1: [" << eigenVectorsPCA(0, 1) << ", " << eigenVectorsPCA(1, 1) << ", " << eigenVectorsPCA(2, 1) << "]" << std::endl;
-  if ( std::abs( eigenVectorsPCA( 0, 1 ) ) < 0.1 &&  sgn<float>( eigenVectorsPCA( 1, 1 ) ) == sgn<float>( eigenVectorsPCA( 2, 1 ) ) )
+  std::cout << "eigen vector 1: [" << eigenVectorsPCA ( 0, 1 ) << ", " << eigenVectorsPCA ( 1, 1 ) << ", " << eigenVectorsPCA ( 2, 1 ) << "]" << std::endl;
+  if ( std::abs( eigenVectorsPCA ( 0, 1 ) ) < 0.1 &&  sgn<float>( eigenVectorsPCA ( 1, 1 ) ) == sgn<float>( eigenVectorsPCA ( 2, 1 ) ) )
   {
-    float y = std::abs( eigenVectorsPCA( 1, 1 ) );
-    float z = std::abs( eigenVectorsPCA( 2, 1 ) );
+    float y = std::abs( eigenVectorsPCA ( 1, 1 ) );
+    float z = std::abs( eigenVectorsPCA ( 2, 1 ) );
     float theta = atan2 ( z, y ) * 180.0 / M_PI + 90.0;
     return theta;
   }
 
-  std::cout << "eigen vector 2: [" << eigenVectorsPCA(0, 2) << ", " << eigenVectorsPCA(1, 2) << ", " << eigenVectorsPCA(2, 2) << "]" << std::endl;
-  if ( std::abs( eigenVectorsPCA( 0, 2 ) ) < 0.1 &&  sgn<float>( eigenVectorsPCA( 1, 2 ) ) == sgn<float>( eigenVectorsPCA( 2, 2 ) ) )
+  std::cout << "eigen vector 2: [" << eigenVectorsPCA ( 0, 2 ) << ", " << eigenVectorsPCA ( 1, 2 ) << ", " << eigenVectorsPCA ( 2, 2 ) << "]" << std::endl;
+  if ( std::abs( eigenVectorsPCA ( 0, 2 ) ) < 0.1 &&  sgn<float>( eigenVectorsPCA ( 1, 2 ) ) == sgn<float>( eigenVectorsPCA ( 2, 2 ) ) )
   {
-    float y = std::abs( eigenVectorsPCA( 1, 2 ) );
-    float z = std::abs( eigenVectorsPCA( 2, 2 ) );
+    float y = std::abs( eigenVectorsPCA ( 1, 2 ) );
+    float z = std::abs( eigenVectorsPCA ( 2, 2 ) );
     float theta = atan2 ( z, y ) * 180.0 / M_PI + 90.0;
     return theta;
   }
@@ -191,57 +194,80 @@ public:
 
   void segment_list_cb ( const object_localizer_msg::Segment_list::ConstPtr& segment_list )
   {
-    if ( segment_list->BBox_list_float.size() > 0 )
+    if ( is_publish_ && segment_list->BBox_list_float.size() > 0 )
     {
+      ofstream do_scan_fs;
+      std::string cfgFileName = ros::package::getPath ( "motion_control" ) + "/config/do_scan.cfg";
+      do_scan_fs.open ( cfgFileName );
+
       // step 1, show all possible segments
-			int box_counter = 0;
+			int bbox_idx = 0;
       for ( object_localizer_msg::BBox_float bbox : segment_list->BBox_list_float )
       {
-        std::cout << "Bounding box [" << box_counter << "] has [x1, x2, y1, y2]: [" << bbox.x1 << "," << bbox.x2 << "," << bbox.y1 << "," << bbox.y2  << "]" << std::endl;
-				box_counter++;
+        // step 2, filter out bounding boxes of possible rivets
+        if ( bbox.x1 > 0.15 && bbox.x2 > 0.15 )
+        {
+          std::cout << "Bounding box [" << bbox_idx << "] has [x1, x2, y1, y2]: [" << bbox.x1 << "," << bbox.x2 << "," << bbox.y1 << "," << bbox.y2  << "]" << std::endl;
+
+          // step 3, generate scan plan for the chosen segment
+          pcl::PCLPointCloud2 pcl_pc2;
+          pcl_conversions::toPCL ( segment_list->Segment_list [ bbox_idx ], pcl_pc2 );
+          PointCloudT::Ptr segment_cloud ( new PointCloudT );
+          pcl::fromPCLPointCloud2 ( pcl_pc2, *segment_cloud );
+          // show_segment_cloud( segment_cloud );
+          // calculate_bounding_box( segment_cloud );
+          Eigen::Vector3f central_point;
+          float theta = calculate_theta ( segment_cloud, central_point );
+          float x_0 = central_point ( 0 );
+          float y_0 = central_point ( 1 );
+          float z_0 = central_point ( 2 );
+
+          float theta_tmp = ( theta - 90.0 ) * M_PI / 180.0;
+          float x_tmp = x_0;
+          float y_tmp = y_0 + scan_offset * std::sin ( theta_tmp );
+          float z_tmp = z_0 - scan_offset * std::cos ( theta_tmp );
+          float x_s = x_tmp;
+          float y_s = y_tmp - scan_distance * std::cos ( theta_tmp );
+          float z_s = z_tmp - scan_distance * std::sin ( theta_tmp );
+          float x_e = x_tmp;
+          float y_e = y_tmp + scan_distance * std::cos ( theta_tmp );
+          float z_e = z_tmp + scan_distance * std::sin ( theta_tmp );
+
+          // step 4, write scanning plannings.
+          std::cout << std::endl << "[***] Rotation around x is [" << theta << "] degrees" << std::endl;
+          // std::cout << "[***] Scan central point is [x, y, z] = [" << x_tmp << ", " << y_tmp << ", " << z_tmp << "]" << std::endl;
+          std::cout << "[***] Scan start point is [x, y, z] = [" << x_s << ", " << y_s << ", " << z_s << "]" << std::endl;
+          std::cout << "[***] Scan end point is [x, y, z] = [" << x_e << ", " << y_e << ", " << z_e << "]" << std::endl << std::endl;
+          do_scan_fs << theta << " " << x_s << " " << y_s << " " << z_s << " " << x_e << " " << y_e << " " << z_e << std::endl;
+        }
+        bbox_idx++;
       }
-
-      // step 2, let user choose to scan which segments
-      std::cout << "Choose a segment to scan >> please enter the id of the segmen <<" << std::endl;
-      int segment_id;
-      std::cin >> segment_id;
-
-      // step 3, generate scan plan for the chosen segment
-      pcl::PCLPointCloud2 pcl_pc2;
-      pcl_conversions::toPCL( segment_list->Segment_list [ segment_id ], pcl_pc2 );
-      PointCloudT::Ptr segment_cloud( new PointCloudT );
-      pcl::fromPCLPointCloud2( pcl_pc2, *segment_cloud );
-      // show_segment_cloud( segment_cloud );
-      // calculate_bounding_box( segment_cloud );
-      Eigen::Vector3f central_point;
-      float theta = calculate_theta( segment_cloud, central_point );
-      float x_0 = central_point(0);
-      float y_0 = central_point(1);
-      float z_0 = central_point(2);
-      // std::cout << "[***] Scan central point is [x, y, z] = [" << x_0 << ", " << y_0 << ", " << z_0 << "]" << std::endl;
-      std::cout << std::endl << "[***] Rotation around x is [" << theta << "] degrees" << std::endl;
-      float theta_tmp = ( theta - 90.0 ) * M_PI / 180.0;
-
-      float x_tmp = x_0;
-      float y_tmp = y_0 + scan_offset * std::sin ( theta_tmp );
-      float z_tmp = z_0 - scan_offset * std::cos ( theta_tmp );
-      float x_s = x_tmp;
-      float y_s = y_tmp - scan_distance * std::cos ( theta_tmp );
-      float z_s = z_tmp - scan_distance * std::sin ( theta_tmp );
-      float x_e = x_tmp;
-      float y_e = y_tmp + scan_distance * std::cos ( theta_tmp );
-      float z_e = z_tmp + scan_distance * std::sin ( theta_tmp );
-
-      // std::cout << "[***] Scan central point is [x, y, z] = [" << x_tmp << ", " << y_tmp << ", " << z_tmp << "]" << std::endl;
-      std::cout << "[***] Scan start point is [x, y, z] = [" << x_s << ", " << y_s << ", " << z_s << "]" << std::endl;
-      std::cout << "[***] Scan end point is [x, y, z] = [" << x_e << ", " << y_e << ", " << z_e << "]" << std::endl << std::endl;
+    	do_scan_fs.close();
+      is_publish_ = false;
     }
   }
 
-  ScanPlanner () : segment_list_topic_ ( "/box_segmenter/segment_list" )
+  bool start_scan_planner ( std_srvs::Empty::Request& req, std_srvs::Empty::Response& res )
   {
-    segment_list_sub_ = nh_.subscribe ( segment_list_topic_, 6, &ScanPlanner::segment_list_cb, this );
-    std::string segment_in_name = nh_.resolveName ( segment_list_topic_ );
+    is_publish_ = true;
+    return true;
+  }
+
+  bool stop_scan_planner ( std_srvs::Empty::Request& req, std_srvs::Empty::Response& res )
+  {
+    is_publish_ = false;
+    return true;
+  }
+
+  ScanPlanner ()
+  {
+    is_publish_ = false;
+    start_scan_planner_ = nh_.advertiseService ( "start_scan_planner", &ScanPlanner::start_scan_planner, this );
+    stop_scan_planner_ = nh_.advertiseService ( "stop_scan_planner", &ScanPlanner::stop_scan_planner, this );
+    ros::Duration ( 1 ) .sleep ();
+
+    std::string segment_in_name = "/box_segmenter/segment_list";
+    segment_list_sub_ = nh_.subscribe ( segment_in_name, 10, &ScanPlanner::segment_list_cb, this );
     ROS_INFO_STREAM ( "Listening for segment list on topic: " << segment_in_name );
   }
 
@@ -249,15 +275,17 @@ public:
 
 private:
   ros::NodeHandle nh_;
-  std::string segment_list_topic_;
+  bool is_publish_;
+  ros::ServiceServer start_scan_planner_, stop_scan_planner_;
   ros::Subscriber segment_list_sub_;
-
 };
 
-int main( int argc, char** argv )
+int main ( int argc, char** argv )
 {
   ros::init ( argc, argv, "scan_planner" );
+  ros::AsyncSpinner spinner ( 4 );
+  spinner.start ();
   ScanPlanner SP;
-  ros::spin ();
+  ros::shutdown ();
   return 0;
 }
