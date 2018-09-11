f32ee08c970bd16f235cb9284be776eb69153756
Yong Wang <wangyongseu9@gmail.com>
2018-09-11 13:37:12 +0200

    add service [start_fix_table_position] and [stop_fix_table_position] to the node [scan_reader]

--- model_loader/src/scan_reader.cpp
+++ model_loader/src/scan_reader.cpp
@@ -6,33 +6,29 @@
 #include <sstream>
 #include <string>
 
-#define PI 3.14159265
-
 #include <ros/ros.h>
 #include <ros/package.h>
-
+#include <std_srvs/Empty.h>
 #include <sensor_msgs/LaserScan.h>
 #include <laser_geometry/laser_geometry.h>
-
 #include <sensor_msgs/PointCloud2.h>
-#include <pcl_ros/point_cloud.h>
-#include <pcl/point_types.h>
-#include <pcl_conversions/pcl_conversions.h>
-#include <pcl/kdtree/kdtree_flann.h>
-
 #include <tf/transform_broadcaster.h>
 #include <tf2_ros/transform_broadcaster.h>
 #include <tf2_ros/static_transform_broadcaster.h>
 #include <tf2/LinearMath/Quaternion.h>
 #include <geometry_msgs/TransformStamped.h>
 
-int msg_counter = 0;
+#include <pcl_ros/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl/kdtree/kdtree_flann.h>
 
+#define PI 3.14159265
+bool is_fix_table_position = false;
 float point1_x = 0.0;
 float point1_y = 0.0;
 float point2_x = 0.0;
 float point2_y = 0.0;
-
 float point1_x_o = 1000000.0;
 float point1_y_o = 1000000.0;
 float point2_x_o = 1000000.0;
@@ -44,10 +40,22 @@ float old_x_t = 10000.0; //cm
 float old_y_t = 10000.0; //cm
 float old_theta2 = 100.0;
 
+void publish_tf ( float x_in, float y_in, float theta2_in )
+{
+  // publish transform between table and floor
+  static tf::TransformBroadcaster br;
+  tf::Transform transform;
+  transform.setOrigin( tf::Vector3( x_in, -y_in, 0.0 ) );
+  tf::Quaternion q;
+  q.setRPY ( 0, 0, theta2_in );
+  transform.setRotation ( q );
+  br.sendTransform ( tf::StampedTransform ( transform, ros::Time::now(), "floor", "table" ) );
+}
+
 void get_the_central_point()
 {
   // from meter to millimeter
-  float x1 = std::round( point1_x * 100.0 );
+  float x1 = std::round ( point1_x * 100.0 );
   if ( point1_x_o == 1000000.0 )
   {
     point1_x_o = x1;
@@ -56,7 +64,7 @@ void get_the_central_point()
     x1 = ( point1_x_o + x1 ) / 2.0;
     point1_x_o = x1;
   }
-  float y1 = std::round( point1_y * 100.0 );
+  float y1 = std::round ( point1_y * 100.0 );
   if ( point1_y_o == 1000000.0 )
   {
     point1_y_o = y1;
@@ -65,7 +73,7 @@ void get_the_central_point()
     y1 = ( point1_y_o + y1 ) / 2.0;
     point1_y_o = y1;
   }
-  float x2 = std::round( point2_x * 100.0 );
+  float x2 = std::round ( point2_x * 100.0 );
   if ( point2_x_o == 1000000.0 )
   {
     point2_x_o = x2;
@@ -74,7 +82,7 @@ void get_the_central_point()
     x2 = ( point2_x_o + x2 ) / 2.0;
     point2_x_o = x2;
   }
-  float y2 = std::round( point2_y * 100.0 );
+  float y2 = std::round ( point2_y * 100.0 );
   if ( point2_y_o == 1000000.0 )
   {
     point2_y_o = y2;
@@ -83,10 +91,10 @@ void get_the_central_point()
     y2 = ( point2_y_o + y2 ) / 2.0;
     point2_y_o = y2;
   }
-  float dist_p1_p2 = std::sqrt( std::pow( x1 - x2, 2 ) + std::pow( y1 - y2, 2 ) );
+  float dist_p1_p2 = std::sqrt ( std::pow ( x1 - x2, 2 ) + std::pow ( y1 - y2, 2 ) );
   // ROS_INFO_STREAM ( "Point1 (p1): [" << x1 << ", " << y1 << "], point2 (p2): [" << x2 << ", " << y2 << "]; Dist(p1, p2):" << dist_p1_p2 );
 
-  if ( std::abs( dist_p1_p2 - 88.0 ) > 3 )
+  if ( std::abs ( dist_p1_p2 - 88.0 ) > 3 )
   {
     // ROS_INFO_STREAM ( "Point1 (p1): [" << x1 << ", " << y1 << "], point2 (p2): [" << x2 << ", " << y2 << "]; Dist(p1, p2):" << dist_p1_p2 );
     // ROS_ERROR_STREAM ( "ERROR -> The robot needs to face the aircraft_frame and have no people in the front." );
@@ -138,29 +146,14 @@ void get_the_central_point()
     old_y_t = y_t;
   }
 
-  std::cout << "Degree = " << theta2 * 180 / PI << " [x_t, y_t] = [" << x_t << ", " << y_t << "]" << std::endl;
-
-  // publish transform between table and floor
-  static tf::TransformBroadcaster br;
-  tf::Transform transform;
   if ( std::abs( old_x_t - x_t ) > 0.05 || std::abs( old_y_t - y_t ) > 0.05 || std::abs( old_theta2 - theta2 ) * 180 / PI > 2 )
   {
-    transform.setOrigin( tf::Vector3(x_t, -y_t, 0.0) );
-    tf::Quaternion q;
-    q.setRPY( 0, 0, theta2 );
-    transform.setRotation( q );
+    std::cout << "New Degree = " << theta2 * 180 / PI << " [x_t, y_t] = [" << x_t << ", " << y_t << "]" << std::endl;
     old_x_t = x_t;
     old_y_t = y_t;
     old_theta2 = theta2;
-  } else
-  {
-    transform.setOrigin( tf::Vector3( old_x_t, -old_y_t, 0.0 ) );
-    tf::Quaternion q;
-    q.setRPY( 0, 0, old_theta2 );
-    transform.setRotation( q );
   }
-  br.sendTransform( tf::StampedTransform(transform, ros::Time::now(), "floor", "table") );
-
+  publish_tf ( old_x_t, -old_y_t, old_theta2 );
 }
 
 laser_geometry::LaserProjection projector_;
@@ -169,6 +162,13 @@ int K = 10;
 
 void scanCallback( const sensor_msgs::LaserScan::ConstPtr& scan_in )
 {
+  if ( is_fix_table_position )
+  {
+    std::cout << "publish old [x, y, theta] " << std::endl;
+    publish_tf ( old_x_t, -old_y_t, old_theta2 );
+    return;
+  }
+
   // convert from laserscan to pcl point cloud
   sensor_msgs::PointCloud2 sensor_pc2;
   projector_.projectLaser( *scan_in, sensor_pc2 );
@@ -233,12 +233,12 @@ void scanCallback( const sensor_msgs::LaserScan::ConstPtr& scan_in )
     searchPoint.x = 0.0;
     searchPoint.y = 0.0;
     searchPoint.z = 0.0;
-    std::vector<int> pointIdxNKNSearch(K);
-    std::vector<float> pointNKNSquaredDistance(K);
+    std::vector<int> pointIdxNKNSearch ( K );
+    std::vector<float> pointNKNSquaredDistance ( K );
 
     std::vector<pcl::PointXYZ> final_vector_left;
     kdtree.setInputCloud ( temp_cloud_left );
-    if ( kdtree.nearestKSearch ( searchPoint, K, pointIdxNKNSearch, pointNKNSquaredDistance) > 0 )
+    if ( kdtree.nearestKSearch ( searchPoint, K, pointIdxNKNSearch, pointNKNSquaredDistance ) > 0 )
     {
       float x_sum = 0.0;
       float y_sum = 0.0;
@@ -281,7 +281,7 @@ void scanCallback( const sensor_msgs::LaserScan::ConstPtr& scan_in )
     //std::cout << final_vector_right.size () << std::endl;
 
     // merge temp_cloud_left and temp_cloud_right and publish the point cloud message
-    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_final (new pcl::PointCloud<pcl::PointXYZ>);
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_final ( new pcl::PointCloud<pcl::PointXYZ> );
     cloud_final->header.frame_id = "scan";
     cloud_final->width = final_vector_left.size () + final_vector_right.size ();
     //std::cout << cloud_final->width << std::endl;
@@ -302,14 +302,35 @@ void scanCallback( const sensor_msgs::LaserScan::ConstPtr& scan_in )
   }
 }
 
-int main( int argc, char **argv )
+bool start_fix_table_position ( std_srvs::Empty::Request& req, std_srvs::Empty::Response& res )
 {
-  ros::init( argc, argv, "scan_reader" );
-  ros::NodeHandle nh;
+  std::cout << "fix table position" << is_fix_table_position << std::endl;
+  is_fix_table_position = true;
+  return true;
+}
 
-  ros::Subscriber sub = nh.subscribe( "/r2000_node/scan", 10, scanCallback );
-  pub = nh.advertise< pcl::PointCloud< pcl::PointXYZ > >( "/scan_reader/points2", 1000 );
-  ros::spin();
+bool stop_fix_table_position ( std_srvs::Empty::Request& req, std_srvs::Empty::Response& res )
+{
+  std::cout << "fix table position" << is_fix_table_position << std::endl;
+  is_fix_table_position = false;
+  return true;
+}
 
+int main ( int argc, char **argv )
+{
+  ros::init ( argc, argv, "scan_reader" );
+  ros::NodeHandle nh_;
+  ros::AsyncSpinner spinner ( 4 );
+  spinner.start();
+
+  is_fix_table_position = false;
+  ros::ServiceServer start_fix_table_position_;
+  start_fix_table_position_ = nh_.advertiseService ( "start_fix_table_position", &start_fix_table_position );
+  ros::ServiceServer stop_fix_table_position_;
+  stop_fix_table_position_ = nh_.advertiseService ( "stop_fix_table_position", &stop_fix_table_position );
+
+  ros::Subscriber sub = nh_.subscribe ( "/r2000_node/scan", 10, scanCallback );
+  pub = nh_.advertise< pcl::PointCloud< pcl::PointXYZ > > ( "/scan_reader/points2", 1000 );
+  ros::waitForShutdown ();
   return 0;
 }
